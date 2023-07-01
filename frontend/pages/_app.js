6ca75b82a4505b45f036f3ffc6b7964fb0459e2e
dinesh11515 <dineshaitham2@gmail.com>
2023-07-01 14:21:00 +0530

    added wagmi,rainbowkit

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,31 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { fantomTestnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+const { chains, publicClient } = configureChains(
+  [fantomTestnet],
+  [publicProvider()]
+);
 
+const { connectors } = getDefaultWallets({
+  appName: "FanSig",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />;
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
